21bfa767c879d782bb0bc3e44fe8b4818a267543
frank-fp <francisco.ferrusca@hotmail.com>
2022-07-03 13:26:37 -0500

    Last edit angular-intro

---
+++ angular-intro/src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+// import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 10000
+  }
+    
+  // get personajes():Personaje[] {
+  //   return this.dbzService.personajes
+  // }
+  
+  constructor(){}
+
+  // agregarNuevoPersonaje(argumento: Personaje){
+  //   // console.log(argumento);
+  //   this.personajes.push(argumento);
+  // }
+}
